86148330b427c19c58c7d3d57576521d8ae1a8a6
Козлов Егор <kozloff130511@gmail.com>
2020-03-03 19:34:19 +0500

    REPCEN-1 правки-провчки

--- src/main/java/com/technoir/repcen/repository/UsersRepository.java
+++ src/main/java/com/technoir/repcen/repository/UsersRepository.java
@@ -3,6 +3,6 @@ package com.technoir.repcen.repository;
 import com.technoir.repcen.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
